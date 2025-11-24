ea83a31003995de2044b777a73a24a4ad28f4f72
achautemps <chautemps.alexandre@gmail.com>
2025-11-24 10:28:34 +0100

    feat(app) : init vue app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <DefaultLayout>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+  </DefaultLayout>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>HP</h1>
+  </main>
+</template>
