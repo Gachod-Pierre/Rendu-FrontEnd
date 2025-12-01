fb3b6b47949b047adc3b120edf48a487f3734600
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-12-01 11:09:16 +0100

    feat(emit) : add emit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import MyButton from '@/components/MyButton.vue';
+import MyLoginForm from '@/components/MyLoginForm.vue';
+
+</script>
+
 <template>
   <main>
     <h1>HP</h1>
+    <MyButton @clickAndHover="() => console.log('Button clicked or hovered')"> Click or Hover me </MyButton>
+    <MyLoginForm />
   </main>
 </template>
